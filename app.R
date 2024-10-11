a7fffd867983515f22aa8ac1558a15b454f76d7f
Mathias Schmolli <m.schmolli@student.tugraz.at>
2024-10-11 18:05:53 +0200

    init commit

---
+++ app.R
@@ -0,0 +1,193 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(stringr)
+library(tsibble)
+library(lubridate)
+library(zoo)
+library(ggplot2)
+library(httr2)
+library(glue)
+library(jsonlite)
+library(dplyr)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Strompreisrechner mit Verbrauchsprofil"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          fileInput(
+            "csv_path",
+            "Verbrauch Upload",
+            multiple = FALSE,
+            accept = NULL,
+            width = NULL,
+            buttonLabel = "Browse...",
+            placeholder = "Keine Datei ausgewählt",
+            capture = NULL
+          ),
+          dateRangeInput(
+            "date_range",
+            "Zeitraum filtern",
+            format = "yyyy-mm-dd",
+            startview = "month",
+            weekstart = 0,
+            language = "de",
+            separator = " bis ",
+            width = NULL,
+            autoclose = TRUE
+          ),
+          textOutput("overall_consumption"),
+          textOutput("overall_payed"),
+          textOutput("average_price"),
+          selectInput( 
+            "select_aggregate", 
+            "Daten zusammenfassen:", 
+            list("Stündlich" = "hourly", "Täglich" = "daily", "Wöchentlich" = "weekly") 
+          ),
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("consumptionPlot"),
+           plotOutput("marketDataPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+    time_series_loaded <- reactive({
+      req(input$csv_path)
+      consumption_data <- read.csv(input$csv_path$datapath, sep=';', skip=1)
+      consumption_data$Verbrauch.Gesamt...1.8.0 <- str_replace(consumption_data$Verbrauch.Gesamt...1.8.0, ",", ".")
+      consumption_data$Verbrauch.Gesamt...1.8.0 <- as.numeric(consumption_data$Verbrauch.Gesamt...1.8.0)
+      consumption_data[is.na(consumption_data)] <- 0
+      
+      consumption_data$Ablesezeitpunkt <- ymd_hms(consumption_data$Ablesezeitpunkt)
+      
+      df_consumption <- data.frame(timestamp = consumption_data$Ablesezeitpunkt, consumption = consumption_data$Verbrauch.Gesamt...1.8.0)
+      df_hourly_data <- df_consumption %>%
+        mutate(date_index = floor_date(timestamp, unit = "hour")) %>%
+        group_by(date_index) %>%
+        summarize(consumption = sum(consumption, na.rm = TRUE))  # Use any summary function here
+      
+      
+      time_series <- as_tsibble(df_hourly_data)
+      
+      start_timestamp <- format(as.numeric(min(time_series$date_index)) * 1000, scientific = FALSE)
+      end_timestamp <- format((as.numeric(max(time_series$date_index)) + 60 * 60) * 1000, scientific = FALSE)
+      
+      req_string <- glue("https://api.awattar.at/v1/marketdata?start={start_timestamp}&end={end_timestamp}")
+      
+      req <- request(req_string)
+      resp <- req_perform(req)
+      
+      market_data_json <- resp |> resp_body_string()
+      
+      market_data <- as.data.frame(fromJSON(market_data_json))
+      market_data$data.marketprice <- market_data$data.marketprice / 10 * 1.2 + 1.44
+      market_data$data.start_timestamp <- as_datetime(market_data$data.start_timestamp / 1000)
+      market_data_time_series <- as_tsibble(market_data[c("data.start_timestamp", "data.marketprice")])
+      
+      time_series$marketprice <- market_data$data.marketprice
+      time_series$amount_payed <- time_series$marketprice * time_series$consumption
+      time_series$aggregated_index <- time_series$date_index
+      
+      updateDateRangeInput(session, "date_range",
+                           start = format_ISO8601(min(time_series$date_index)),
+                           end = format_ISO8601(max(time_series$date_index)),
+                           min = format_ISO8601(min(time_series$date_index)),
+                           max = format_ISO8601(max(time_series$date_index)),
+        )
+
+      return(time_series)
+      })
+
+  
+    time_series_filtered <- reactive({
+      time_series_loaded() %>% filter_index(format_ISO8601(input$date_range[1]) ~ format_ISO8601(input$date_range[2]))
+    })
+    
+    time_series_aggregated <- reactive({
+      switch(input$select_aggregate, 
+             hourly={
+               return(time_series_filtered())
+             },
+             daily={
+               time_series_filtered() %>% index_by(aggregated_index = ~as_date(.)) %>% summarise(
+                 consumption = sum(consumption),
+                 amount_payed = sum(amount_payed),
+                 marketprice = mean(marketprice)
+               )  
+             },
+             weekly={
+               time_series_filtered() %>% index_by(aggregated_index = ~yearweek(.)) %>% summarise(
+                 consumption = sum(consumption),
+                 amount_payed = sum(amount_payed),
+                 marketprice = mean(marketprice)
+               )  
+             },
+             {
+               return(time_series_filtered())
+             }
+        )
+    })
+    
+    market_data_filtered <- reactive({
+      filter_index(market_data_time_series, format_ISO8601(input$date_range[1]) ~ format_ISO8601(input$date_range[2]))
+    })
+    
+    output$overall_consumption <- renderText({
+      paste("Gesamtverbrauch", format(round(sum(time_series_filtered()$consumption), 2), nsmall = 2), "kWh")
+    })
+    
+    output$overall_payed <- renderText({
+      paste("Energiekosten", format(round(sum(time_series_filtered()$amount_payed) / 100, 2), nsmall = 2), "€")
+    })
+    
+    output$average_price <- renderText({
+      paste("Durchnittspreis", format(round(sum(time_series_filtered()$amount_payed) / sum(time_series_filtered()$consumption), 2), nsmall = 2), "Cent / kWh")
+    })
+
+    output$consumptionPlot <- renderPlot({
+        # draw the histogram with the specified number of bins
+      ggplot(time_series_aggregated()) + 
+        aes(x = aggregated_index) +
+        geom_point(aes(y=marketprice,colour="Marktpreis (Cent / kWh)"), shape=1) +
+        geom_line(aes(y=marketprice,colour="Marktpreis (Cent / kWh)")) +
+        geom_point(aes(y=amount_payed,colour="Bezahlter Betrag (Cent, Verbrauch * Marktpreis)"), shape=1) +
+        geom_line(aes(y=amount_payed,colour="Bezahlter Betrag (Cent, Verbrauch * Marktpreis)"), size=1.2) + 
+        theme_linedraw() + 
+        theme(legend.position = "top") +
+        labs(title = "Verbrauchsprofil und Marktpreise", y= "Cent", x = "Datum / Uhrzeit") +
+        scale_color_manual(values = c("blue", "darkgray"))
+    })
+    
+    output$marketDataPlot <- renderPlot({
+      # draw the histogram with the specified number of bins
+      ggplot(time_series_aggregated()) + 
+        aes(x = aggregated_index) +
+        geom_point(aes(y=consumption)) +
+        geom_line(aes(y=consumption)) +
+        theme_linedraw() + 
+        labs(title = "Verbrauch", y= "Verbrauch (kwH)", x = "Datum / Uhrzeit")
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
